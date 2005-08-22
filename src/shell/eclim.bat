b4156aa81e397c44f834189c8ef098c189e8ed05
ervandew <ervandew@gmail.com>
2005-08-22 05:07:35 +0000

    windows batch file version of eclim scripts (untested)
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@17 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/shell/eclim.bat
@@ -0,0 +1,35 @@
+@echo off
+
+rem Copyright (c) 2004 - 2005
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem      http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+rem
+rem Author: Eric Van Dewoestine
+
+if "%ECLIPSE_HOME%" == "" goto no_eclipse_home
+
+setlocal EnableDelayedExpansion
+set ECLIM_HOME=%ECLIPSE_HOME%\plugins\org.eclim_${eclim.version}
+
+set JAVA_CP=.
+for %%i in ("%ECLIM_HOME%\*.jar") do set JAVA_CP=!JAVA_CP!;%%i
+endlocal & set JAVA_CP=%JAVA_CP%
+
+echo java -cp "%JAVA_CP%" org.eclim.client.Main %*
+goto exit
+
+:no_eclipse_home
+  echo ECLIPSE_HOME not set
+  goto exit
+
+:exit
---
+++ src/shell/eclimd.bat
@@ -0,0 +1,36 @@
+@echo off
+
+rem Copyright (c) 2004 - 2005
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem      http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+rem
+rem Author: Eric Van Dewoestine
+
+if "%ECLIPSE_HOME%" == "" goto no_eclipse_home
+
+if not exist "%ECLIPSE_HOME%\eclipse" goto no_eclipse_executable
+
+cd %ECLIPSE_HOME%
+
+eclipse -debug -consolelog -nosplash -application org.eclim.application -vmargs -Dorg.eclim.spring-factory.xml=org/eclim/server/spring-factory.xml
+goto exit
+
+:no_eclipse_home
+  echo ECLIPSE_HOME not set
+  goto exit
+
+:no_eclipse_executable
+  echo No eclipse executable found.  ECLIPSE_HOME may be invalid.
+  goto exit
+
+:exit
