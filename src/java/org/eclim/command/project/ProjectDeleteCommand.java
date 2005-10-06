5470f2d97120ceb0bfc9bdc4873051864a06b6ef
ervandew <ervandew@gmail.com>
2005-10-06 05:12:23 +0000

    added project delete command
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@75 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/java/org/eclim/command/project/ProjectDeleteCommand.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2004 - 2005
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.command.project;
+
+import java.io.IOException;
+
+import org.eclim.Services;
+
+import org.eclim.client.Options;
+
+import org.eclim.command.AbstractCommand;
+import org.eclim.command.CommandLine;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+
+/**
+ * Command to delete a project.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class ProjectDeleteCommand
+  extends AbstractCommand
+{
+  /**
+   * {@inheritDoc}
+   */
+  public Object execute (CommandLine _commandLine)
+    throws IOException
+  {
+    try{
+      String name = _commandLine.getValue(Options.NAME_OPTION);
+      IProject project =
+        ResourcesPlugin.getWorkspace().getRoot().getProject(name);
+      if(project.exists()){
+        project.delete(false/*deleteContent*/, true/*force*/, null/*monitor*/);
+        return Services.getMessage("project.deleted", new Object[]{name});
+      }
+      return Services.getMessage("project.not.found", new Object[]{name});
+    }catch(Throwable t){
+      return t;
+    }
+  }
+}
