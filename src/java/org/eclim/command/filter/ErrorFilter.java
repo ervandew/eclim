f76c4e048e380a631d22900000ca4197b4a080f3
ervandew <ervandew@gmail.com>
2005-11-26 19:22:26 +0000

    moved PropertiesFilter and ProjectUpdateFilter to re-usable PositionFilter and ErrorFilter
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@197 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/java/org/eclim/command/filter/ErrorFilter.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2004 - 2005
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.command.filter;
+
+import org.eclim.command.Error;
+import org.eclim.command.OutputFilter;
+
+/**
+ * Filter for generating vim output for an array of Error.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class ErrorFilter
+  implements OutputFilter
+{
+  /**
+   * {@inheritDoc}
+   */
+  public String filter (Object _result)
+  {
+    StringBuffer buffer = new StringBuffer();
+    Error[] errors = (Error[])_result;
+    for(int ii = 0; ii < errors.length; ii++){
+      if(ii > 0){
+        buffer.append('\n');
+      }
+      buffer.append(errors[ii].getFilename())
+        .append('|')
+        .append(errors[ii].getLineNumber())
+        .append(" col ")
+        .append(errors[ii].getColumnNumber())
+        .append('|')
+        .append(errors[ii].getMessage());
+    }
+
+    return buffer.toString();
+  }
+}
---
+++ src/java/org/eclim/command/filter/PositionFilter.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2004 - 2005
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.command.filter;
+
+import org.eclim.command.OutputFilter;
+
+import org.eclim.util.file.Position;
+
+import org.eclim.util.vim.VimUtils;
+
+/**
+ * Filter for Position.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class PositionFilter
+  implements OutputFilter
+{
+  /**
+   * {@inheritDoc}
+   */
+  public String filter (Object _result)
+  {
+    Position position = (Position)_result;
+    return VimUtils.translateStartEnd(position);
+  }
+}
