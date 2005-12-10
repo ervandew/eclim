b32aff0b9b50f67ad4814a937ea3ed9c6206f341
ervandew <ervandew@gmail.com>
2005-12-10 02:35:48 +0000

    re-adding ConsoleAdapter and a non-jboss version of LoggerStream
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@224 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/java/org/eclim/util/logging/ConsoleAppender.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) 2004 - 2005
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.util.logging;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Writer;
+
+/**
+ * Extension to log4j's ConsoleAppender that prevents looping when setting
+ * System.out or System.err to use log4j.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class ConsoleAppender
+  extends org.apache.log4j.ConsoleAppender
+{
+  private boolean writerSet;
+
+  /**
+   * Prepares the appender for use.
+   */
+  public void activateOptions()
+  {
+    if(getTarget().equals(SYSTEM_ERR)) {
+      setWriter(createWriter(new SystemStream(System.err)));
+    } else {
+      setWriter(createWriter(new SystemStream(System.out)));
+    }
+
+    super.activateOptions();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public synchronized void setWriter (Writer _writer)
+  {
+    if(!writerSet){
+      super.setWriter(_writer);
+      writerSet = true;
+    }
+  }
+
+  /**
+   * An implementation of OutputStream that redirects to the
+   * stream supplied at construction.
+   */
+  private static class SystemStream
+    extends OutputStream
+  {
+    private OutputStream out;
+
+    public SystemStream (OutputStream _out)
+    {
+      out = _out;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void close ()
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void flush ()
+      throws IOException
+    {
+      out.flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void write (final byte[] _b)
+      throws IOException
+    {
+      out.write(_b);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void write (final byte[] _b, final int _off, final int _len)
+      throws IOException
+    {
+      out.write(_b, _off, _len);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void write (final int _b)
+      throws IOException
+    {
+      out.write(_b);
+    }
+  }
+}
