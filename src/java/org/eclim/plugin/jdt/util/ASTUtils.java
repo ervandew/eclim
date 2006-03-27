5cd9347518d8ad539dfc95e01e47058e44282fd0
ervandew <ervandew@gmail.com>
2006-03-27 05:47:28 +0000

    added couple more findNode methods
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@351 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/java/org/eclim/plugin/jdt/util/ASTUtils.java
+++ src/java/org/eclim/plugin/jdt/util/ASTUtils.java
@@ -15,11 +15,23 @@
  */
 package org.eclim.plugin.jdt.util;
 
+import org.apache.log4j.Logger;
+
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
 
 import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.Initializer;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.PackageDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+import org.eclipse.jdt.internal.corext.dom.NodeFinder;
 
 import org.eclipse.jface.text.Document;
 
@@ -33,6 +45,8 @@ import org.eclipse.text.edits.TextEdit;
  */
 public class ASTUtils
 {
+  private static final Logger logger = Logger.getLogger(ASTUtils.class);
+
   private ASTUtils ()
   {
   }
@@ -92,5 +106,90 @@ public class ASTUtils
     edits.apply(document);
     _src.getBuffer().setContents(document.get());
     _src.save(null, false);
+  }
+
+  /**
+   * Finds the node at the specified offset.
+   *
+   * @param _cu The CompilationUnit.
+   * @param _offset The node offset in the compilation unit.
+   * @return The node at the specified offset.
+   */
+  public static ASTNode findNode (CompilationUnit _cu, int _offset)
+    throws Exception
+  {
+    NodeFinder finder= new NodeFinder(_offset, 1);
+    _cu.accept(finder);
+    //return finder.getCoveredNode();
+    return finder.getCoveringNode();
+  }
+
+  /**
+   * Finds the node at the specified offset that matches up with the supplied
+   * IJavaElement.
+   *
+   * @param _cu The CompilationUnit.
+   * @param _offset The node offset in the compilation unit.
+   * @param _element The IJavaElement to match.
+   * @return The node at the specified offset.
+   */
+  public static ASTNode findNode (
+      CompilationUnit _cu, int _offset, IJavaElement _element)
+    throws Exception
+  {
+    ASTNode node = findNode(_cu, _offset);
+    if(node == null){
+      return null;
+    }
+
+    if(_element.getElementType() == IJavaElement.TYPE_PARAMETER){
+      _element = _element.getParent();
+    }
+
+    switch(_element.getElementType()){
+      case IJavaElement.PACKAGE_DECLARATION:
+        node = resolveNode(node, PackageDeclaration.class);
+        break;
+      case IJavaElement.IMPORT_DECLARATION:
+        node = resolveNode(node, ImportDeclaration.class);
+        break;
+      case IJavaElement.TYPE:
+        node = resolveNode(node, TypeDeclaration.class);
+        break;
+      case IJavaElement.INITIALIZER:
+        node = resolveNode(node, Initializer.class);
+        break;
+      case IJavaElement.FIELD:
+        node = resolveNode(node, FieldDeclaration.class);
+        break;
+      case IJavaElement.METHOD:
+        node = resolveNode(node, MethodDeclaration.class);
+        break;
+      default:
+        logger.info("findNode(CompilationUnit,int,IJavaElement) - " +
+            "unrecognized element type " + _element.getElementType());
+    }
+    return node;
+  }
+
+  /**
+   * Walk up the node tree until a node of the specified type is reached.
+   *
+   * @param _node The starting node.
+   * @param _type The type to resolve.
+   * @return The resulting node.
+   */
+  private static ASTNode resolveNode (ASTNode _node, Class _type)
+    throws Exception
+  {
+    if(_node == null){
+      return null;
+    }
+
+    if(_type.isAssignableFrom(_node.getClass())){
+      return _node;
+    }
+
+    return resolveNode(_node.getParent(), _type);
   }
 }
