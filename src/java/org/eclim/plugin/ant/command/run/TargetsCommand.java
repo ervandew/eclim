e958b727129d3bb64c4e072d0dafa04db002f278
ervandew <ervandew@gmail.com>
2006-03-30 02:58:15 +0000

    added :Ant command to run ant from any file and command completion of targets
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@378 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/java/org/eclim/plugin/ant/command/run/TargetsCommand.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2004 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.plugin.ant.command.run;
+
+import java.io.IOException;
+
+import java.text.Collator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.tools.ant.Project;
+
+import org.eclim.command.AbstractCommand;
+import org.eclim.command.CommandLine;
+import org.eclim.command.Options;
+
+import org.eclim.plugin.ant.util.AntUtils;
+
+import org.eclipse.ant.internal.ui.model.AntModel;
+import org.eclipse.ant.internal.ui.model.AntProjectNode;
+
+/**
+ * Command to handle request for available tasks in an ant build file.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class TargetsCommand
+  extends AbstractCommand
+{
+  /**
+   * {@inheritDoc}
+   */
+  public Object execute (CommandLine _commandLine)
+    throws IOException
+  {
+    List results = new ArrayList();
+    try{
+      String project = _commandLine.getValue(Options.PROJECT_OPTION);
+      String file = _commandLine.getValue(Options.FILE_OPTION);
+
+      AntModel model = (AntModel)AntUtils.getAntModel(file);
+      AntProjectNode projectNode = model.getProjectNode(true);
+      Project antProject = projectNode.getProject();
+
+      Map targets = antProject.getTargets();
+      for (Iterator ii = targets.keySet().iterator(); ii.hasNext();){
+        String target = (String)ii.next();
+        if(target.trim().length() > 0){
+          results.add(target);
+        }
+      }
+
+      Collections.sort(results, Collator.getInstance());
+      return filter(_commandLine, results);
+    }catch(Exception e){
+      return e;
+    }
+  }
+}
---
+++ src/java/org/eclim/plugin/ant/command/run/TargetsFilter.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2004 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.plugin.ant.command.run;
+
+import java.util.List;
+
+import org.eclim.command.OutputFilter;
+
+/**
+ * Filter for command to get ant targets.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class TargetsFilter
+  implements OutputFilter
+{
+  /**
+   * {@inheritDoc}
+   */
+  public String filter (Object _result)
+  {
+    StringBuffer buffer = new StringBuffer();
+    if(_result != null){
+      List targets = (List)_result;
+      for (int ii = 0; ii < targets.size(); ii++){
+        if(buffer.length() > 0){
+          buffer.append('\n');
+        }
+        String target = (String)targets.get(ii);
+        buffer.append(target);
+      }
+    }
+    return buffer.toString();
+  }
+}
