d2b111a447a899878838511bd7c774ac46a6408d
ervandew <ervandew@gmail.com>
2006-04-05 01:02:08 +0000

    new utility class for working with scripts
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@399 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/java/org/eclim/util/ScriptUtils.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2004 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.util;
+
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+
+import java.util.Map;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyShell;
+
+import groovy.text.SimpleTemplateEngine;
+import groovy.text.Template;
+
+import org.eclim.Services;
+
+import org.eclim.plugin.PluginResources;
+
+/**
+ * Utility classes for working with scripts.
+ * <p/>
+ * Currently all scripts are expected to be implemented in groovy.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class ScriptUtils
+{
+  /**
+   * Evaluates the specified script and returns the result.
+   *
+   * @param _resources The plugin resources.
+   * @param _script The script.
+   * @param _values Any variable name / value pairs for the script.
+   * @return The result of evaluating the supplied script.
+   */
+  public static Object evaluateScript (
+      PluginResources _resources, String _script, Map _values)
+    throws Exception
+  {
+    Binding binding = new Binding(_values);
+    GroovyShell shell = new GroovyShell(binding);
+
+    try{
+      return shell.evaluate(_resources.getResourceAsStream(_script));
+    }catch(NullPointerException npe){
+      throw new IllegalArgumentException(
+          Services.getMessage("script.not.found", _script), npe);
+    }
+  }
+
+  /**
+   * Parses the names script from the supplied PluginResources and returns the
+   * Class that can be used to create instances to invoke methods on.
+   *
+   * @param _resources The plugin resources.
+   * @param _script The script.
+   * @return
+   */
+  public static Class parseClass (PluginResources _resources, String _script)
+    throws Exception
+  {
+    GroovyClassLoader gcl = new GroovyClassLoader();
+    try{
+      return gcl.parseClass(_resources.getResourceAsStream(_script));
+    }catch(NullPointerException npe){
+      throw new IllegalArgumentException(
+          Services.getMessage("script.not.found", _script), npe);
+    }
+  }
+}
