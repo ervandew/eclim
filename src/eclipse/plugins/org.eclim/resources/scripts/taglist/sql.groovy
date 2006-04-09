bf98c679ef1f32819fbfbdb734b183519384cb66
ervandew <ervandew@gmail.com>
2006-04-09 23:59:43 +0000

    taglist script for sql files
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@428 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/eclipse/plugins/org.eclim/resources/scripts/taglist/sql.groovy
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.regex.Pattern;
+
+import org.eclim.command.taglist.RegexTaglist;
+import org.eclim.command.taglist.TaglistScript;
+import org.eclim.command.taglist.TagResult;
+
+/**
+ * Processes tags for sql files.
+ */
+class ForrestDocumentTags implements TaglistScript
+{
+  public TagResult[] execute (String file)
+  {
+    def g = Character.valueOf('g' as char);
+    def u = Character.valueOf('u' as char);
+    def p = Character.valueOf('p' as char);
+    def s = Character.valueOf('s' as char);
+    def t = Character.valueOf('t' as char);
+    def v = Character.valueOf('v' as char);
+    def q = Character.valueOf('q' as char);
+    def f = Character.valueOf('f' as char);
+    def r = Character.valueOf('r' as char);
+    def m = Character.valueOf('m' as char);
+    def z = Character.valueOf('z' as char);
+
+    def regex = null;
+    try{
+      regex = new RegexTaglist(file);
+
+      def pattern = Pattern.compile(
+        "(s?)create\\s+(group|role)\\s+([a-zA-Z0-9_.]+)", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(g, pattern, "\$3");
+
+      pattern = Pattern.compile(
+        "(s?)create\\s+user\\s+([a-zA-Z0-9_.]+)", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(u, pattern, "\$2");
+
+      pattern = Pattern.compile(
+        "(s?)create\\s+(tablespace|dbspace)\\s+([a-zA-Z0-9_.]+)", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(p, pattern, "\$3");
+
+      pattern = Pattern.compile(
+        "(s?)create\\s+schema\\s+([a-zA-Z0-9_.]+)", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(s, pattern, "\$2");
+
+      pattern = Pattern.compile(
+        "(s?)create\\s+table\\s+([a-zA-Z0-9_.]+)", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(t, pattern, "\$2");
+
+      pattern = Pattern.compile(
+        "(s?)create\\s+view\\s+([a-zA-Z0-9_.]+)", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(v, pattern, "\$2");
+
+      pattern = Pattern.compile(
+        "(s?)create\\s+sequence\\s+([a-zA-Z0-9_.]+)", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(q, pattern, "\$2");
+
+      pattern = Pattern.compile(
+        "(s?)create(\\s+or\\s+replace)?\\s+function\\s+([a-zA-Z0-9_.]+)", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(f, pattern, "\$3");
+
+      pattern = Pattern.compile(
+        "(s?)exec\\s+sp_addrole\\s+['\"]([a-zA-Z0-9_.]+)['\"]", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(r, pattern, "\$2");
+
+      pattern = Pattern.compile(
+        "(s?)exec\\s+sp_addlogin\\s+@loginname=['\"](.*?)['\"]", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(m, pattern, "\$2");
+
+      pattern = Pattern.compile(
+        "(s?)alter\\s+database.*add\\s+filegroup\\s+([a-zA-Z0-9_.]+)", Pattern.CASE_INSENSITIVE);
+      regex.addPattern(z, pattern, "\$2");
+
+      return regex.execute();
+    }finally{
+      regex.close();
+    }
+  }
+}
