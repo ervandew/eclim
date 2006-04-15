b6944f61118bd481cb32ed6b2681ad2e3e9ddde1
ervandew <ervandew@gmail.com>
2006-04-15 01:10:34 +0000

    ping response is now eclim and the version number
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@446 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/java/org/eclim/command/admin/PingCommand.java
+++ src/java/org/eclim/command/admin/PingCommand.java
@@ -17,8 +17,12 @@ package org.eclim.command.admin;
 
 import java.io.IOException;
 
+import org.eclim.Services;
+
 import org.eclim.command.AbstractCommand;
 import org.eclim.command.CommandLine;
+
+import org.eclim.plugin.PluginResources;
 
 /**
  * Pings the server.
@@ -35,6 +39,8 @@ public class PingCommand
   public Object execute (CommandLine _commandLine)
     throws IOException
   {
-    return "PingResponse";
+    PluginResources resources = Services.getPluginResources();
+    return resources.getProperty("pluginName") + " " +
+      resources.getProperty("pluginVersion");
   }
 }
