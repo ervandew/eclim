8633ed34bd48a2936cf215450e156f8898f01fd3
ervandew <ervandew@gmail.com>
2006-04-26 00:46:08 +0000

    now redirecting stderr to stdout since, at least on linux, the stderr is no longer returned via vim's system call
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@469 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/shell/eclim.bat
+++ src/shell/eclim.bat
@@ -19,4 +19,4 @@ rem Author: Eric Van Dewoestine
 set CUR_PATH=%~p0
 set JAVA_CMD=%CUR_PATH%\ng --nailgun-port 9091
 
-%JAVA_CMD% org.eclim.command.Main %*
+%JAVA_CMD% org.eclim.command.Main %* 2>&1
