18838b95d341564c0447db8b802fb5c8a2e34a4c
ervandew <ervandew@gmail.com>
2006-07-20 18:48:24 +0000

    started adding support for a wiki
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@693 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/forrest/wiki/local/config.php
@@ -0,0 +1,20 @@
+<?php if (!defined('PmWiki')) exit();
+
+##  $WikiTitle is the name that appears in the browser's title bar.
+$WikiTitle = 'Eclim Wiki';
+
+## Writable place for session data
+session_save_path('/tmp/persistent/eclim/pmwiki.d/sessions');
+
+## Store wiki pages outside the document tree.
+$WorkDir = '/tmp/persistent/eclim/pmwiki.d/wiki.d';
+$WikiDir = new PageStore('/tmp/eclim/projectname/pmwiki.d/wiki.d/$FullName');
+
+$DefaultPasswords['admin']='$1$zlC1Ya1G$5paaylTMDCveyhwfQQuWc1';
+
+## Config to help facilitate backups.
+## Add a custom page storage location for wiki pages.
+$PageStorePath = "wikilib2.d/\$FullName";
+$where = count($WikiLibDirs);
+if ($where>1) $where--;
+array_splice($WikiLibDirs, $where, 0, array(new PageStore($PageStorePath)));
