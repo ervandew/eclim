6ae0cf23237c4bbce9c9241f6dc9ddbbe17c1354
ervandew <ervandew@gmail.com>
2006-08-25 02:41:29 +0000

    Test case for Settings
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@777 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/test/junit/org/eclim/command/admin/SettingsCommandTest.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.command.admin;
+
+import java.io.ByteArrayInputStream;
+
+import java.util.Properties;
+
+import junit.framework.JUnit4TestAdapter;
+
+import org.eclim.Eclim;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test case for SettingsCommand.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class SettingsCommandTest
+{
+  /**
+   * Test the command.
+   */
+  @Test
+  public void execute ()
+    throws Exception
+  {
+    String result = Eclim.execute(new String[]{"-command", "settings"});
+
+    Properties properties = new Properties();
+    properties.load(new ByteArrayInputStream(result.getBytes()));
+
+    assertTrue("Missing org.eclim.user.email",
+        properties.containsKey("org.eclim.user.email"));
+    assertTrue("Missing org.eclim.user.name",
+        properties.containsKey("org.eclim.user.name"));
+
+    assertTrue("Missing org.eclim.project.copyright",
+        properties.containsKey("org.eclim.project.copyright"));
+    assertTrue("Missing org.eclim.project.version",
+        properties.containsKey("org.eclim.project.version"));
+
+    assertTrue("Missing org.eclim.java.logging.impl",
+        properties.containsKey("org.eclim.java.logging.impl"));
+    assertTrue("Missing org.eclim.java.validation.ignore.warnings",
+        properties.containsKey("org.eclim.java.validation.ignore.warnings"));
+    assertTrue("Missing org.eclipse.jdt.core.compiler.source",
+        properties.containsKey("org.eclipse.jdt.core.compiler.source"));
+
+    assertTrue("Missing org.eclim.java.doc.version",
+        properties.containsKey("org.eclim.java.doc.version"));
+  }
+
+  /**
+   * Required for running tests in ant 1.6.5.
+   */
+  public static junit.framework.Test suite()
+  {
+    return new JUnit4TestAdapter(SettingsCommandTest.class);
+  }
+}
