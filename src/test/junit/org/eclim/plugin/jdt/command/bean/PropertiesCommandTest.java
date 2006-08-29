f2b4811064f0596351eb22b616afcb9ac779cbf0
ervandew <ervandew@gmail.com>
2006-08-29 16:26:53 +0000

    PropertiesCommand testing
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@806 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/test/junit/org/eclim/plugin/jdt/command/bean/PropertiesCommandTest.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.plugin.jdt.command.bean;
+
+import java.util.regex.Pattern;
+
+import org.eclim.Eclim;
+import org.eclim.Jdt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test case for PropertiesCommand.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class PropertiesCommandTest
+{
+  private static final String TEST_FILE =
+    "src/org/eclim/test/bean/TestBean.java";
+
+  @Test
+  public void executeGet ()
+  {
+    assertTrue("Java project doesn't exist.",
+        Eclim.projectExists(Jdt.TEST_PROJECT));
+
+    Eclim.execute(new String[]{
+      "java_bean_properties", "-p", Jdt.TEST_PROJECT,
+      "-f", Eclim.resolveFile(Jdt.TEST_PROJECT, TEST_FILE),
+      "-o", "1", "-t", "getter", "-r", "name"
+    });
+
+    String contents = Eclim.fileToString(Jdt.TEST_PROJECT, TEST_FILE);
+    assertTrue("Getter not found.",
+        Pattern.compile("public String getName \\(\\)").matcher(contents).find());
+  }
+
+  @Test
+  public void executeSet ()
+  {
+    assertTrue("Java project doesn't exist.",
+        Eclim.projectExists(Jdt.TEST_PROJECT));
+
+    Eclim.execute(new String[]{
+      "java_bean_properties", "-p", Jdt.TEST_PROJECT,
+      "-f", Eclim.resolveFile(Jdt.TEST_PROJECT, TEST_FILE),
+      "-o", "1", "-t", "setter", "-r", "name"
+    });
+
+    String contents = Eclim.fileToString(Jdt.TEST_PROJECT, TEST_FILE);
+    assertTrue("Setter not found.",
+        Pattern.compile("public void setName \\(String name\\)")
+        .matcher(contents).find());
+  }
+
+  @Test
+  public void executeGetSet ()
+  {
+    assertTrue("Java project doesn't exist.",
+        Eclim.projectExists(Jdt.TEST_PROJECT));
+
+    Eclim.execute(new String[]{
+      "java_bean_properties", "-p", Jdt.TEST_PROJECT,
+      "-f", Eclim.resolveFile(Jdt.TEST_PROJECT, TEST_FILE),
+      "-o", "1", "-t", "getter_setter", "-r", "valid"
+    });
+
+    String contents = Eclim.fileToString(Jdt.TEST_PROJECT, TEST_FILE);
+    assertTrue("Getter not found.",
+        Pattern.compile("public boolean isValid \\(\\)").matcher(contents).find());
+    assertTrue("Setter not found.",
+        Pattern.compile("public void setValid \\(boolean valid\\)")
+        .matcher(contents).find());
+  }
+
+  /**
+   * Required for running tests in ant 1.6.5.
+   */
+  public static junit.framework.Test suite()
+  {
+    return new junit.framework.JUnit4TestAdapter(PropertiesCommandTest.class);
+  }
+}
---
+++ src/test/resources/eclim_unit_test_java/src/org/eclim/test/bean/TestBean.java
@@ -0,0 +1,7 @@
+package org.eclim.test.bean;
+
+public class TestBean
+{
+  private String name;
+  private boolean valid;
+}
