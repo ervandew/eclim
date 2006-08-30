c636417fc0b6256410640ad9d81f6a815599c028
ervandew <ervandew@gmail.com>
2006-08-30 04:28:59 +0000

    added test case for ImportCommand and UnusedImportsCommand
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@819 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/test/junit/org/eclim/plugin/jdt/command/include/ImportCommandTest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.plugin.jdt.command.include;
+
+import org.eclim.Eclim;
+
+import org.eclim.plugin.jdt.Jdt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test case for ImportCommand.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class ImportCommandTest
+{
+  @Test
+  public void execute ()
+  {
+    assertTrue("Java project doesn't exist.",
+        Eclim.projectExists(Jdt.TEST_PROJECT));
+
+    String result = Eclim.execute(new String[]{
+      "java_import", "-n", Jdt.TEST_PROJECT, "-p", "List"
+    });
+
+    System.out.println(result);
+
+    assertEquals("Wrong results", "java.awt.List\njava.util.List", result);
+  }
+
+  /**
+   * Required for running tests in ant 1.6.5.
+   */
+  public static junit.framework.Test suite()
+  {
+    return new junit.framework.JUnit4TestAdapter(ImportCommandTest.class);
+  }
+}
---
+++ src/test/resources/eclim_unit_test_java/src/org/eclim/test/include/TestUnusedImport.java
@@ -0,0 +1,11 @@
+package org.eclim.test.include;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestUnusedImport
+{
+  private Map map = new HashMap();
+}
