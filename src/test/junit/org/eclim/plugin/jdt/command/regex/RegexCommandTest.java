402ca5a4865ed40cf66959f511a4c91bb55ea34c
ervandew <ervandew@gmail.com>
2006-09-04 21:26:19 +0000

    test case for RegexCommand
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@862 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/test/junit/org/eclim/plugin/jdt/command/regex/RegexCommandTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.plugin.jdt.command.regex;
+
+import org.eclim.Eclim;
+
+import org.eclim.plugin.jdt.Jdt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test case for RegexCommand.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class RegexCommandTest
+{
+  private static final String TEST_FILE =
+    "src/org/eclim/test/regex/test_regex.txt";
+
+  @Test
+  public void execute ()
+  {
+    assertTrue("Java project doesn't exist.",
+        Eclim.projectExists(Jdt.TEST_PROJECT));
+
+    String result = Eclim.execute(new String[]{
+      "java_regex", "-f", Eclim.resolveFile(Jdt.TEST_PROJECT, TEST_FILE)
+    });
+
+    System.out.println(result);
+
+    assertEquals("Wrong result", "3:6-3:10\n3:22-3:26", result);
+  }
+
+  /**
+   * Required for running tests in ant 1.6.5.
+   */
+  public static junit.framework.Test suite()
+  {
+    return new junit.framework.JUnit4TestAdapter(RegexCommandTest.class);
+  }
+}
