f488c19c3c3e2be17cd88f3c08e6674e47e04cc9
ervandew <ervandew@gmail.com>
2006-09-05 00:02:16 +0000

    added test case for ant ValidateCommand
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@867 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/test/junit/org/eclim/plugin/ant/command/validate/ValidateCommandTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.plugin.ant.command.validate;
+
+import org.eclim.Eclim;
+
+import org.eclim.plugin.ant.Ant;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test case for ValidateCommand.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class ValidateCommandTest
+{
+  private static final String TEST_FILE = "build.xml";
+
+  @Test
+  public void execute ()
+  {
+    String result = Eclim.execute(new String[]{
+      "ant_validate", "-p", Ant.TEST_PROJECT,
+      "-f", Eclim.resolveFile(Ant.TEST_PROJECT, TEST_FILE)
+    });
+
+    System.out.println(result);
+
+    assertEquals("Wrong result.",
+        Eclim.resolveFile(Ant.TEST_PROJECT, TEST_FILE) +
+        "|5 col 2|Default target none does not exist in this project|e",
+        result);
+  }
+
+  /**
+   * Required for running tests in ant 1.6.5.
+   */
+  public static junit.framework.Test suite()
+  {
+    return new junit.framework.JUnit4TestAdapter(ValidateCommandTest.class);
+  }
+}
