73a022213a4207bf2d9116c44320c0aef2564c1b
ervandew <ervandew@gmail.com>
2006-10-10 01:36:04 +0000

    new xml test cases
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@981 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/test/junit/org/eclim/command/xml/format/FormatCommandTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.command.xml.format;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.eclim.Eclim;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test FormatCommand.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class FormatCommandTest
+{
+  private static final String TEST_FILE = "xml/format.xml";
+
+  @Test
+  public void execute ()
+  {
+    String result = Eclim.execute(new String[]{
+      "xml_format",
+      "-f", Eclim.resolveFile(TEST_FILE), "-w", "80", "-i", "2"
+    });
+    System.out.println(result);
+
+    String[] lines = StringUtils.split(result, '\n');
+
+    assertEquals("Wrong number of lines.", 6, lines.length);
+
+    assertEquals("<blah attr1=\"one\" attr2=\"two\" attr3=\"three\" attr4=\"four\" attr5=\"five\"", lines[1]);
+    assertEquals("  attr6=\"six\" attr7=\"seven\">", lines[2]);
+    assertEquals("  <one>one</one>", lines[3]);
+    assertEquals("  <two/>", lines[4]);
+    assertEquals("</blah>", lines[5]);
+  }
+
+  /**
+   * Required for running tests in ant 1.6.5.
+   */
+  public static junit.framework.Test suite()
+  {
+    return new junit.framework.JUnit4TestAdapter(FormatCommandTest.class);
+  }
+}
---
+++ src/test/junit/org/eclim/command/xml/validate/ValidateCommandTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.command.xml.validate;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.eclim.Eclim;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test ValidateCommand.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class ValidateCommandTest
+{
+  private static final String TEST_FILE = "xml/test_dtd.xml";
+
+  @Test
+  public void execute ()
+  {
+    String file = Eclim.resolveFile(TEST_FILE);
+    String result = Eclim.execute(new String[]{"xml_validate", "-f", file });
+    System.out.println(result);
+
+    String[] results = StringUtils.split(result, '\n');
+
+    assertEquals("Wrong number of errors.", 2, results.length);
+
+    assertTrue("Wrong filename.", results[0].startsWith(file));
+    assertTrue("Wrong filename.", results[1].startsWith(file));
+
+    assertTrue("Wrong error.",
+        results[0].indexOf("The content of element type \"bar\" is incomplete") != -1);
+    assertTrue("Wrong error.",
+        results[1].indexOf("The content of element type \"foo\" must match") != -1);
+  }
+
+  /**
+   * Required for running tests in ant 1.6.5.
+   */
+  public static junit.framework.Test suite()
+  {
+    return new junit.framework.JUnit4TestAdapter(ValidateCommandTest.class);
+  }
+}
