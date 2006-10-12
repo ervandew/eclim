e259708475a329897149204e667823d7d98c708c
ervandew <ervandew@gmail.com>
2006-10-12 03:08:31 +0000

    started eclim installer
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1003 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/installer/java/org/eclim/installer/step/EclipseStep.java
@@ -0,0 +1,105 @@
+package org.eclim.installer.step;
+
+import java.io.File;
+
+import java.util.Properties;
+
+import org.apache.commons.io.FilenameUtils;
+
+import org.apache.tools.ant.taskdefs.condition.Os;
+
+import org.formic.form.Validator;
+
+import org.formic.form.validator.AggregateValidator;
+
+import org.formic.wizard.step.FileChooserStep;
+
+/**
+ * Step for specifying location of eclipse installation.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class EclipseStep
+  extends FileChooserStep
+{
+  private static final String ICON = "/resources/images/eclipse.png";
+
+  /**
+   * Constructs the welcome step.
+   */
+  public EclipseStep (String name)
+  {
+    super(name);
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.formic.wizard.WizardStep#initProperties(Properties)
+   */
+  public void initProperties (Properties properties)
+  {
+    properties.put(PROPERTY, "eclipseHome");
+    properties.put("selectionMode", "directories");
+    super.initProperties(properties);
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see AbstractStep#getIconPath()
+   */
+  protected String getIconPath ()
+  {
+    return ICON;
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see FileChooserStep#getValidator()
+   */
+  protected Validator getValidator ()
+  {
+    AggregateValidator validator = new AggregateValidator();
+    validator.addValidator(super.getValidator());
+    validator.addValidator(new EclipseHomeValidator());
+
+    return validator;
+  }
+
+  /**
+   * Validator for validating eclipse home directory.
+   */
+  private class EclipseHomeValidator
+    implements Validator
+  {
+    /**
+     * {@inheritDoc}
+     * @see Validator#isValid(Object)
+     */
+    public boolean isValid (Object value)
+    {
+      String folder = (String)value;
+      if(folder != null && folder.trim().length() > 0){
+        String exe = null;
+        if(Os.isFamily("windows")){
+          exe = FilenameUtils.concat(folder, "eclipse.exe");
+        }else{
+          exe = FilenameUtils.concat(folder, "eclipse");
+        }
+
+        File file = new File(exe);
+        return file.exists() && file.isFile();
+      }
+      return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see Validator#getErrorMessage()
+     */
+    public String getErrorMessage ()
+    {
+      return getName() + ".eclipseHome.invalid";
+    }
+  }
+}
