5d952fcb1c8cfbdecb63370b4b14b23ae1b57859
ervandew <ervandew@gmail.com>
2006-11-03 06:23:31 +0000

    added dtd validation support
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1058 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/java/org/eclim/plugin/wst/command/validate/DtdValidateCommand.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.plugin.wst.command.validate;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.eclim.command.AbstractCommand;
+import org.eclim.command.CommandLine;
+import org.eclim.command.Error;
+import org.eclim.command.Options;
+
+import org.eclipse.wst.dtd.core.internal.validation.DTDValidator;
+
+import org.eclipse.wst.xml.core.internal.validation.core.ValidationMessage;
+import org.eclipse.wst.xml.core.internal.validation.core.ValidationReport;
+
+/**
+ * Command to validate dtd files.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class DtdValidateCommand
+  extends AbstractCommand
+{
+  private static final String URI_PREFIX = "file://";
+
+  /**
+   * {@inheritDoc}
+   */
+  public Object execute (CommandLine _commandLine)
+  {
+    try{
+      String project = _commandLine.getValue(Options.PROJECT_OPTION);
+      String file = _commandLine.getValue(Options.FILE_OPTION);
+
+      ArrayList results = new ArrayList();
+      DTDValidator validator = new DTDValidator();
+      ValidationReport result = validator.validate(URI_PREFIX + file);
+      ValidationMessage[] messages = result.getValidationMessages();
+      for (int ii = 0; ii < messages.length; ii++){
+        StringBuffer message = new StringBuffer(messages[ii].getMessage());
+        for (Iterator jj = messages[ii].getNestedMessages().iterator(); jj.hasNext();){
+          ValidationMessage nested = (ValidationMessage)jj.next();
+          message.append(' ').append(nested.getMessage());
+        }
+
+        results.add(new Error(
+              message.toString(),
+              messages[ii].getUri().substring(URI_PREFIX.length()),
+              messages[ii].getLineNumber(),
+              messages[ii].getColumnNumber(),
+              false//messages[ii].getSeverity() != ValidationMessage.SEV_HIGH
+        ));
+      }
+
+      return super.filter(_commandLine,
+          (Error[])results.toArray(new Error[results.size()]));
+    }catch(Throwable t){
+      return t;
+    }
+  }
+}
