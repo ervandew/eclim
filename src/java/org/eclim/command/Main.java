9c86d99a81ff8beb2465695badd01a88e091b321
ervandew <ervandew@gmail.com>
2006-11-04 03:26:26 +0000

    added some debug statements
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1059 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/java/org/eclim/command/Main.java
+++ src/java/org/eclim/command/Main.java
@@ -19,6 +19,8 @@ import org.apache.commons.cli.ParseException;
 
 import org.apache.commons.lang.StringUtils;
 
+import org.apache.log4j.Logger;
+
 import org.eclim.Services;
 
 import org.eclipse.swt.widgets.EclimDisplay;
@@ -31,6 +33,8 @@ import org.eclipse.swt.widgets.EclimDisplay;
  */
 public class Main
 {
+  private static final Logger logger = Logger.getLogger(Main.class);
+
   /**
    * Main method for executing the client.
    *
@@ -39,6 +43,8 @@ public class Main
   public static final void main (String[] _args)
   {
     try{
+      logger.debug("Main - enter");
+
       // set dummy display's current thread
       ((EclimDisplay)org.eclipse.swt.widgets.Display.getDefault())
         .setThread(Thread.currentThread());
@@ -50,15 +56,18 @@ public class Main
       }catch(ParseException e){
         System.out.println(
             Services.getMessage(e.getClass().getName(), e.getMessage()));
+        logger.debug("Main - exit on error");
         System.exit(1);
       }
 
       if(commandLine.hasOption(Options.HELP_OPTION)){
         options.usage(commandLine.getValue(Options.HELP_OPTION));
+        logger.debug("Main - exit");
         System.exit(0);
       }
 
       String commandName = commandLine.getValue(Options.COMMAND_OPTION);
+      logger.debug("Main - command: {}", commandName);
       if(commandName == null || commandName.trim().equals(StringUtils.EMPTY)){
         throw new IllegalArgumentException(
             Services.getMessage("command.required"));
@@ -70,13 +79,17 @@ public class Main
 
       if(result instanceof Throwable){
         ((Throwable)result).printStackTrace();
+        logger.debug("Main - exit on error");
         System.exit(1);
       }else{
         System.out.println(result);
       }
     }catch(Exception e){
       e.printStackTrace();
+
+      logger.debug("Main - exit on error");
       System.exit(1);
     }
+    logger.debug("Main - exit");
   }
 }
