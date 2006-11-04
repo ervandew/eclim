0356c7b5fbcb9a19cb9e387d04f864eb955caf84
ervandew <ervandew@gmail.com>
2006-11-04 04:35:25 +0000

    moved dtd and html taglist scripts to wst plugin
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1061 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/eclipse/plugins/org.eclim.wst/resources/scripts/taglist/dtd.groovy
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2005 - 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+
+import org.eclim.command.taglist.TaglistScript;
+import org.eclim.command.taglist.TagResult;
+
+/**
+ * Processes tags for dtd files.
+ */
+class DtdTags implements TaglistScript
+{
+  public TagResult[] execute (String file)
+  {
+    def results = [];
+    def lineNumber = 0;
+    new File(file).eachLine {
+      line -> processTag(line, ++lineNumber, file, results)
+    };
+
+    return (TagResult[])results.toArray(new TagResult[results.size()]);
+  }
+
+  void processTag (line, lineNumber, file, results)
+  {
+    def matcher = line =~ /^\s*<!ELEMENT\s+(.*?)(\s|\s*$)/;
+    if(matcher.matches()){
+      def name = matcher[0][1];
+      def tag = new TagResult(
+        file:file, pattern:line, line:lineNumber, kind:'e', name:name);
+
+      results.add(tag);
+    }
+  }
+}
