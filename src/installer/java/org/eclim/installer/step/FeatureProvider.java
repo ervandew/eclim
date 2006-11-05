08dbe806614bfc4074109a424156d2d04d7013ff
ervandew <ervandew@gmail.com>
2006-11-05 23:33:26 +0000

    FeatureListStep now requires FeatureProvider class instead of xml file
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1068 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/installer/java/org/eclim/installer/step/FeatureProvider.java
@@ -0,0 +1,72 @@
+package org.eclim.installer.step;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.formic.form.console.ConsoleForm;
+
+import org.formic.form.gui.GuiForm;
+
+import org.formic.wizard.step.FeatureListStep.Feature;
+
+import org.formic.wizard.step.FeatureListStep;
+
+/**
+ * Provider to supply avaiable features to FeatureListStep.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class FeatureProvider
+  implements FeatureListStep.FeatureProvider, PropertyChangeListener
+{
+  private static final String[] FEATURES =
+    {"ant", "maven", "jdt", "wst"};
+
+  private static final boolean[] FEATURES_ENABLED =
+    {true, true, true, false};
+
+  private GuiForm guiForm;
+  private ConsoleForm consoleForm;
+
+  /**
+   * {@inheritDoc}
+   * @see FeatureListStep.FeatureProvider#getFeatures()
+   */
+  public Feature[] getFeatures ()
+  {
+    Feature[] features = new Feature[FEATURES.length];
+    for (int ii = 0; ii < features.length; ii++){
+      features[ii] = new Feature(FEATURES[ii], FEATURES_ENABLED[ii]);
+    }
+
+    return features;
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see FeatureListStep.FeatureProvider#setGuiForm(GuiForm)
+   */
+  public void setGuiForm (GuiForm form)
+  {
+    this.guiForm = form;
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see FeatureListStep.FeatureProvider#setConsoleForm(ConsoleForm)
+   */
+  public void setConsoleForm (ConsoleForm form)
+  {
+    this.consoleForm = form;
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see PropertyChangeListener#propertyChange(PropertyChangeEvent)
+   */
+  public void propertyChange (PropertyChangeEvent evt)
+  {
+    // do nothing for now.
+  }
+}
