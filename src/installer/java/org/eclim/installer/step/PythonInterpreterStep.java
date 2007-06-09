a5d6fdb2a6048d85a9e269d36a1b767f811dddb1
ervandew <ervandew@gmail.com>
2007-06-09 16:56:19 +0000

    increased timeout to 10 seconds
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1241 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/installer/java/org/eclim/installer/step/PythonInterpreterStep.java
+++ src/installer/java/org/eclim/installer/step/PythonInterpreterStep.java
@@ -278,7 +278,7 @@ public class PythonInterpreterStep
       }
       cmd[1] = "list";
 
-      CommandExecutor executor = CommandExecutor.execute(cmd, 5000);
+      CommandExecutor executor = CommandExecutor.execute(cmd, 10 * 1000);
       ArrayList interpreters = new ArrayList();
       Pattern pattern = Pattern.compile("^(/|\\w:).*");
       if(executor.getReturnCode() == 0){
