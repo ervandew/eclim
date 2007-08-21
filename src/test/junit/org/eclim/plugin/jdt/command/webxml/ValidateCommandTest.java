ca9c4754f2b54d5c5dad1e608eadb89d69869f20
ervandew <ervandew@gmail.com>
2007-08-21 04:53:09 +0000

    added unit test for web.xml validation
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1421 b5bd0a2e-1a23-0410-8567-9814cbbba304

---
+++ src/test/junit/org/eclim/plugin/jdt/command/webxml/ValidateCommandTest.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2005 - 2007
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclim.plugin.jdt.command.webxml;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.eclim.Eclim;
+
+import org.eclim.plugin.jdt.Jdt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test case for ValidateCommand.
+ *
+ * @author Eric Van Dewoestine
+ * @version $Revision$
+ */
+public class ValidateCommandTest
+{
+  private static final String TEST_FILE = "webxml/web.xml";
+
+  @Test
+  public void validateXmlErrors ()
+  {
+    String result = Eclim.execute(new String[]{
+      "webxml_validate", "-p", Jdt.TEST_PROJECT, "-f", TEST_FILE
+    });
+
+    System.out.println(result);
+
+    String[] results = StringUtils.split(result, '\n');
+    assertEquals("Wrong number of results", 3, results.length);
+
+    assertEquals("Wrong result.",
+        Eclim.resolveFile(Jdt.TEST_PROJECT, TEST_FILE) +
+        "|12 col 1|Class 'foo.bar.Listener' not found in project 'eclim_unit_test_java'.|e",
+        results[0]);
+
+    assertEquals("Wrong result.",
+        Eclim.resolveFile(Jdt.TEST_PROJECT, TEST_FILE) +
+        "|22 col 1|Class 'org.apache.solr.servlet.SolrServlet' not found in project 'eclim_unit_test_java'.|e",
+        results[1]);
+
+    assertEquals("Wrong result.",
+        Eclim.resolveFile(Jdt.TEST_PROJECT, TEST_FILE) +
+        "|32 col 1|No servlet definition with name 'pong' defined.|e",
+        results[2]);
+  }
+
+  /**
+   * Required for running tests in ant 1.6.5.
+   */
+  public static junit.framework.Test suite()
+  {
+    return new junit.framework.JUnit4TestAdapter(ValidateCommandTest.class);
+  }
+}
