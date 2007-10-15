6d3c60d05cb7fb8debbb3ff81efa95bfeda2e4ac
ervandew <ervandew@gmail.com>
2007-10-15 01:23:57 +0000

    working version for pdt 1.0.0 final
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1527 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/java/org/eclim/plugin/pdt/command/src/SrcUpdateCommand.java
+++ src/java/org/eclim/plugin/pdt/command/src/SrcUpdateCommand.java
@@ -26,6 +26,11 @@ import org.eclim.command.Options;
 
 import org.eclim.command.filter.ErrorFilter;
 
+import org.eclim.eclipse.EclimPlugin;
+
+import org.eclim.plugin.wst.command.validate.Reporter;
+import org.eclim.plugin.wst.command.validate.ValidationContext;
+
 import org.eclim.util.ProjectUtils;
 
 import org.eclim.util.file.FileOffsets;
@@ -33,12 +38,21 @@ import org.eclim.util.file.FileOffsets;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 
-import org.eclipse.php.internal.core.documentModel.validate.PHPProblemsValidator;
+import org.eclipse.jface.text.reconciler.DirtyRegion;
 
 import org.eclipse.php.internal.core.phpModel.parser.PHPWorkspaceModelManager;
 
-import org.eclipse.php.internal.core.phpModel.phpElementData.IPHPMarker;
-import org.eclipse.php.internal.core.phpModel.phpElementData.PHPFileData;
+import org.eclipse.php.internal.ui.editor.PHPStructuredTextViewer;
+
+import org.eclipse.php.internal.ui.editor.validation.PHPValidator;
+
+import org.eclipse.wst.sse.core.StructuredModelManager;
+
+import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
+
+import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocument;
+
+import org.eclipse.wst.validation.internal.provisional.core.IMessage;
 
 /**
  * Command to update and optionally validate a php src file.
@@ -49,6 +63,8 @@ import org.eclipse.php.internal.core.phpModel.phpElementData.PHPFileData;
 public class SrcUpdateCommand
   extends AbstractCommand
 {
+  private static final PHPValidator VALIDATOR = new PHPValidator();
+
   /**
    * {@inheritDoc}
    */
@@ -61,36 +77,45 @@ public class SrcUpdateCommand
     IProject project = ProjectUtils.getProject(projectName, true);
     IFile ifile = ProjectUtils.getFile(project, file);
 
+    // ensure model is refreshed with latest version of the file.
+    PHPWorkspaceModelManager.getInstance().addFileToModel(ifile);
+
     // validate the src file.
     if(_commandLine.hasOption(Options.VALIDATE_OPTION)){
-      // ensure model is refreshed with latest version of the file.
-      PHPWorkspaceModelManager.getInstance().addFileToModel(ifile);
+      // force loading of PHPProjectModel
+      PHPWorkspaceModelManager.getInstance().getModelForProject(project, true);
 
-      PHPProblemsValidator validator = PHPProblemsValidator.getInstance();
-      validator.validateFileProblems(ifile, true);
+      String filepath = ProjectUtils.getFilePath(project, file);
+      Reporter reporter = new Reporter();
+      IStructuredModel model =
+        StructuredModelManager.getModelManager().getModelForRead(ifile);
+      IStructuredDocument document = model.getStructuredDocument();
 
-      PHPFileData fileData = PHPWorkspaceModelManager.getInstance()
-        .getModelForFile(ifile.getFullPath().toString(), true);
-      IPHPMarker[] markers = fileData.getMarkers();
+      PHPStructuredTextViewer viewer = new PHPStructuredTextViewer(
+          EclimPlugin.getShell(), null, null, false, 0);
+      viewer.setDocument(document);
+      viewer.setSelectedRange(0, 20);
+      DirtyRegion region = new DirtyRegion(
+          0, document.getLength(), DirtyRegion.INSERT, "");
+      ValidationContext context = new ValidationContext(filepath);
 
-      if(markers.length > 0){
-        String filepath = ProjectUtils.getFilePath(project, file);
-        FileOffsets offsets = FileOffsets.compile(filepath);
-        ArrayList<Error> errors = new ArrayList<Error>();
-        for(int ii = 0; ii < markers.length; ii++){
-          IPHPMarker marker = markers[ii];
-          int[] lineColumn = offsets.offsetToLineColumn(
-              marker.getUserData().getStartPosition());
-          errors.add(new Error(
-              marker.getDescription(),
-              filepath,
-              lineColumn[0],
-              lineColumn[1],
-              !IPHPMarker.ERROR.equals(marker.getType())
-          ));
-        }
-        return ErrorFilter.instance.filter(_commandLine, errors);
+      VALIDATOR.connect(document);
+      VALIDATOR.validate(region, context, reporter);
+      VALIDATOR.disconnect(document);
+
+      FileOffsets offsets = FileOffsets.compile(filepath);
+      ArrayList<Error> errors = new ArrayList<Error>();
+      for(IMessage message : reporter.getMessages()){
+        int[] lineColumn = offsets.offsetToLineColumn(message.getOffset());
+        errors.add(new Error(
+            message.getText(),
+            filepath,
+            lineColumn[0],
+            lineColumn[1],
+            message.getSeverity() != IMessage.HIGH_SEVERITY
+        ));
       }
+      return ErrorFilter.instance.filter(_commandLine, errors);
     }
     return StringUtils.EMPTY;
   }
