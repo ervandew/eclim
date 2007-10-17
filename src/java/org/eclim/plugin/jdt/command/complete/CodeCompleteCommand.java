1c5ac31bd6d1fff6f79171cb7d4aeccbb36c8d7e
ervandew <ervandew@gmail.com>
2007-10-17 04:13:35 +0000

    fixed possible string index error
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1545 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/java/org/eclim/plugin/jdt/command/complete/CodeCompleteCommand.java
+++ src/java/org/eclim/plugin/jdt/command/complete/CodeCompleteCommand.java
@@ -107,7 +107,8 @@ public class CodeCompleteCommand
       case CompletionProposal.METHOD_REF:
         // trim off the trailing paren if the method takes any arguments.
         if (displayString.lastIndexOf(')') > displayString.lastIndexOf('(') + 1 &&
-            completion.charAt(completion.length() - 1) == ')')
+            (completion.length() > 0 &&
+             completion.charAt(completion.length() - 1) == ')'))
         {
           completion = completion.substring(0, completion.length() - 1);
         }
