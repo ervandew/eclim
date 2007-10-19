ced726ba7881312af2b2c51e449468dda738e090
ervandew <ervandew@gmail.com>
2007-10-19 01:52:03 +0000

    installer now validates vim, eclipse, and make
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1551 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/installer/java/org/eclim/installer/step/RequirementProvider.java
+++ src/installer/java/org/eclim/installer/step/RequirementProvider.java
@@ -15,13 +15,27 @@
  */
 package org.eclim.installer.step;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+
+import org.apache.commons.lang.StringUtils;
+
 import org.formic.Installer;
 
 import org.formic.form.console.ConsoleForm;
 
 import org.formic.form.gui.GuiForm;
 
+import org.formic.util.CommandExecutor;
+
 import org.formic.wizard.step.RequirementsValidationStep.Requirement;
+import org.formic.wizard.step.RequirementsValidationStep.ValidatingRequirement;
 
 import org.formic.wizard.step.RequirementsValidationStep;
 
@@ -49,8 +63,10 @@ public class RequirementProvider
    */
   public Requirement[] getRequirements ()
   {
-    Requirement[] requirements = new Requirement[1];
-    requirements[0] = new Requirement("make");
+    Requirement[] requirements = new Requirement[3];
+    requirements[0] = new EclipseRequirement();
+    requirements[1] = new VimRequirement();
+    requirements[2] = new MakeRequirement();
     return requirements;
   }
 
@@ -60,26 +76,8 @@ public class RequirementProvider
    */
   public Status validate (Requirement requirement)
   {
-    String eclipseHome = (String)
-      Installer.getContext().getValue("eclipse.home");
-
-    if ("make".equals(requirement.getKey())){
-      try{
-        int result = Runtime.getRuntime().exec(
-            new String[] {"which", "make"}).waitFor();
-        if (result != 0){
-          return new Status(
-              FAIL, Installer.getString("make.not.found"));
-        }
-      }catch(Exception e){
-        logger.error("Error checking for 'make'", e);
-        return new Status(
-            WARN, Installer.getString("make.validation.failed"));
-      }
-    }
-
-
-    return OK_STATUS;
+    ValidatingRequirement req = (ValidatingRequirement)requirement;
+    return req.validate();
   }
 
   /**
@@ -98,5 +96,134 @@ public class RequirementProvider
   public void setConsoleForm (ConsoleForm form)
   {
     this.consoleForm = form;
+  }
+
+  private class EclipseRequirement
+    extends ValidatingRequirement
+  {
+    private final Pattern VERSION =
+      Pattern.compile(".*Release\\s+(([0-9]+\\.)+[0-9]).*");
+
+    public EclipseRequirement ()
+    {
+      super("eclipse");
+    }
+
+    public Status validate ()
+    {
+      String eclipseHome = (String)
+        Installer.getContext().getValue("eclipse.home");
+      eclipseHome = eclipseHome.replace('\\', '/');
+      // probably a better way
+      File file = new File(eclipseHome + "/readme/readme_eclipse.html");
+      if (!file.exists()){
+        logger.error(
+            "Error checking eclipse version. File does not exist: ", file);
+        return new Status(
+            WARN, Installer.getString("eclipse.validation.failed"));
+      }
+      BufferedReader reader = null;
+      try{
+        reader = new BufferedReader(new FileReader(file));
+        String line = null;
+        for(int ii = 0; ii < 30; ii++){
+          line = reader.readLine();
+          Matcher matcher = VERSION.matcher(line);
+          if (matcher.matches()){
+            String version = matcher.group(1);
+            String[] parts = StringUtils.split(version, '.');
+            int major = Integer.parseInt(parts[0]);
+            int minor = Integer.parseInt(parts[1]);
+            int patch = parts.length > 2 ? Integer.parseInt(parts[2]) : 0;
+
+            if(major < 3 || minor < 3 || patch < 0){
+              return new Status(FAIL,
+                  Installer.getString("eclipse.version.invalid", version, "3.3.x"));
+            }
+            break;
+          }
+        }
+      }catch(Exception e){
+        logger.error("Error checking eclipse version.", e);
+        return new Status(
+            WARN, Installer.getString("eclipse.validation.failed"));
+      }finally{
+        IOUtils.closeQuietly(reader);
+      }
+      return OK_STATUS;
+    }
+  }
+
+  private class VimRequirement
+    extends ValidatingRequirement
+  {
+    private final Pattern VERSION =
+      Pattern.compile(".*VIM - Vi IMproved (([0-9]+\\.)+[0-9]).*");
+
+    public VimRequirement ()
+    {
+      super("vim");
+    }
+
+    public Status validate ()
+    {
+      try{
+        CommandExecutor command =
+          CommandExecutor.execute(new String[]{"vim", "--version"}, 2000);
+        if (command.getReturnCode() != 0){
+          logger.error(
+              "Error checking vim version: {}", command.getErrorMessage());
+          return new Status(
+              WARN, Installer.getString("vim.validation.failed"));
+        }
+        String result = command.getResult();
+        Matcher matcher = VERSION.matcher(result);
+        if(!matcher.find()){
+          logger.error("Error finding vim version in output: {}", result);
+          return new Status(
+              WARN, Installer.getString("vim.validation.failed"));
+        }
+        String version = matcher.group(1);
+        String[] parts = StringUtils.split(version, '.');
+        int major = Integer.parseInt(parts[0]);
+        int minor = Integer.parseInt(parts[1]);
+        int patch = parts.length > 2 ? Integer.parseInt(parts[2]) : 0;
+
+        if(major < 7 || minor < 1 || patch < 0){
+          return new Status(FAIL,
+              Installer.getString("vim.version.invalid", version, "7.1.x"));
+        }
+      }catch(Exception e){
+        logger.error("Error checking vim version.", e);
+        return new Status(WARN, Installer.getString("vim.validation.failed"));
+      }
+      return OK_STATUS;
+    }
+  }
+
+  private class MakeRequirement
+    extends ValidatingRequirement
+  {
+    public MakeRequirement ()
+    {
+      super("make");
+    }
+
+    public Status validate ()
+    {
+      try{
+        int result = Runtime.getRuntime().exec(
+            new String[] {"which", "make"}).waitFor();
+        if (result != 0){
+          return new Status(
+              FAIL, Installer.getString("make.not.found"));
+        }
+      }catch(Exception e){
+        logger.error("Error checking for 'make'", e);
+        return new Status(
+            WARN, Installer.getString("make.validation.failed"));
+      }
+      return OK_STATUS;
+    }
   }
 }
