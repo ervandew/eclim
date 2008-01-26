a32660fa01bca08a141135164e297b3b1f04d24b
ervandew <ervandew@gmail.com>
2008-01-26 02:09:05 +0000

    fixed spliting of methods option
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1691 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/java/org/eclim/plugin/jdt/command/impl/ImplCommand.java
+++ src/java/org/eclim/plugin/jdt/command/impl/ImplCommand.java
@@ -83,7 +83,7 @@ public class ImplCommand
 
       String[] methods = null;
       if(methodsOption != null){
-        methods = StringUtils.split(methodsOption, ",,");
+        methods = StringUtils.splitByWholeSeparator(methodsOption, ",,");
       }else{
         methods = getUnimplementedMethods(type, superType);
       }
