d1cc4af9918a12b608eacd1c5b870dd964aaa0d0
ervandew <ervandew@gmail.com>
2008-03-25 04:55:27 +0000

    eclim license changed from apache 2 to GPLv3
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@1803 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/ant/xsl/forrest_site-google_sitemap.xsl
+++ src/ant/xsl/forrest_site-google_sitemap.xsl
@@ -1,11 +1,25 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
+<!--
+  Copyright (C) 2005 - 2008  Eric Van Dewoestine
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
 
 <!--
-  - Stylesheet which processes a forest site.xml file to produce an xml file
-  - for use with googles sitemap service.
-  -
-  - Author: Eric Van Dewoestine
-  -->
+  Stylesheet which processes a forest site.xml file to produce an xml file
+  for use with googles sitemap service.
+-->
 <xsl:stylesheet version="1.0"
     xmlns="http://www.google.com/schemas/sitemap/0.84"
     xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
--- src/eclipse/plugins/org.eclim.installer/bin/install.bat
+++ src/eclipse/plugins/org.eclim.installer/bin/install.bat
@@ -1,20 +1,19 @@
 @echo off
 
-rem Copyright (c) 2005 - 2008
+rem Copyright (C) 2005 - 2008  Eric Van Dewoestine
 rem
-rem Licensed under the Apache License, Version 2.0 (the "License");
-rem you may not use this file except in compliance with the License.
-rem You may obtain a copy of the License at
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
 rem
-rem      http://www.apache.org/licenses/LICENSE-2.0
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
 rem
-rem Unless required by applicable law or agreed to in writing, software
-rem distributed under the License is distributed on an "AS IS" BASIS,
-rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-rem See the License for the specific language governing permissions and
-rem limitations under the License.
-rem
-rem Author: Eric Van Dewoestine
+rem You should have received a copy of the GNU General Public License
+rem along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 set ECLIPSE_HOME=%~dp0\..\..\..
 "%ECLIPSE_HOME%\eclipse" -nosplash -clean -refresh -application org.eclim.installer.application %*
