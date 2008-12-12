f93936cc21239cdd0a9e6b0ce72b2b98c8931a2c
ervandew <ervandew@gmail.com>
2008-12-12 02:49:58 +0000

    updated project create and update to support 2.0 (dltk) buildpath
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@2178 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/java/org/eclim/plugin/pdt/project/PhpProjectManager.java
+++ src/java/org/eclim/plugin/pdt/project/PhpProjectManager.java
@@ -16,37 +16,46 @@
  */
 package org.eclim.plugin.pdt.project;
 
-import java.io.File;
+import java.io.FileInputStream;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
 
 import org.eclim.Services;
 
 import org.eclim.command.CommandLine;
 import org.eclim.command.Error;
+import org.eclim.command.Options;
 
 import org.eclim.plugin.pdt.PluginResources;
 
 import org.eclim.project.ProjectManager;
 
+import org.eclim.util.IOUtils;
 import org.eclim.util.ProjectUtils;
 import org.eclim.util.XmlUtils;
 
-import org.eclim.util.file.FileUtils;
+import org.eclim.util.file.FileOffsets;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 
-import org.eclipse.php.internal.core.phpModel.parser.PHPIncludePathModelManager;
-import org.eclipse.php.internal.core.phpModel.parser.PHPProjectModel;
-import org.eclipse.php.internal.core.phpModel.parser.PHPWorkspaceModelManager;
+import org.eclipse.dltk.core.DLTKCore;
+import org.eclipse.dltk.core.IBuildpathEntry;
+import org.eclipse.dltk.core.IDLTKLanguageToolkit;
+import org.eclipse.dltk.core.IModelStatus;
+import org.eclipse.dltk.core.IScriptProject;
 
-import org.eclipse.php.internal.core.project.IIncludePathEntry;
-import org.eclipse.php.internal.core.project.PHPNature;
+import org.eclipse.dltk.internal.core.BuildpathEntry;
 
-import org.eclipse.php.internal.core.project.options.PHPProjectOptions;
+import org.eclipse.dltk.internal.ui.wizards.BuildpathDetector;
+
+import org.eclipse.php.internal.core.PHPLanguageToolkit;
 
 /**
  * Implementation of {@link ProjectManager} for php projects.
@@ -57,8 +66,10 @@ import org.eclipse.php.internal.core.project.options.PHPProjectOptions;
 public class PhpProjectManager
   implements ProjectManager
 {
-  private static final String PROJECT_OPTIONS_XSD =
-    "/resources/schema/eclipse/projectOptions.xsd";
+  private static final String BUILDPATH_XSD =
+    "/resources/schema/eclipse/buildpath.xsd";
+
+  private static final Pattern STATUS_PATTERN = Pattern.compile(".*: '(.*)'.*");
 
   /**
    * {@inheritDoc}
@@ -66,7 +77,23 @@ public class PhpProjectManager
   public void create (IProject _project, CommandLine _commandLine)
     throws Exception
   {
-    refresh(_project, _commandLine);
+    String dependsString = _commandLine.getValue(Options.DEPENDS_OPTION);
+
+    IScriptProject scriptProject = DLTKCore.create(_project);
+    IDLTKLanguageToolkit toolkit = PHPLanguageToolkit.getDefault();
+    BuildpathDetector detector = new BuildpathDetector(_project, toolkit);
+    detector.detectBuildpath(null);
+    IBuildpathEntry[] detected = detector.getBuildpath();
+    IBuildpathEntry[] depends =
+      createOrUpdateDependencies(scriptProject, dependsString);
+
+    IBuildpathEntry[] buildpath = merge(
+        new IBuildpathEntry[][]{detected, depends});
+          //scriptProject.readRawClasspath(), detected, depends, container
+
+    scriptProject.setRawBuildpath(buildpath, null);
+    scriptProject.makeConsistent(null);
+    scriptProject.save(null, false);
   }
 
   /**
@@ -75,56 +102,45 @@ public class PhpProjectManager
   public List<Error> update (IProject _project, CommandLine _commandLine)
     throws Exception
   {
-    // validate that .classpath xml is well formed and valid.
+    IScriptProject scriptProject = DLTKCore.create(_project);
+    scriptProject.getResource().refreshLocal(IResource.DEPTH_INFINITE, null);
+
+    // validate that .buildpath xml is well formed and valid.
     PluginResources resources = (PluginResources)
       Services.getPluginResources(PluginResources.NAME);
     List<Error> errors = XmlUtils.validateXml(
-        _project.getName(),
-        ".projectOptions",
-        resources.getResource(PROJECT_OPTIONS_XSD).toString());
+        scriptProject.getProject().getName(),
+        ".buildpath",
+        resources.getResource(BUILDPATH_XSD).toString());
     if(errors.size() > 0){
       return errors;
     }
 
-    // force the .projectOptions file to be refreshed.
-    _project.refreshLocal(IResource.DEPTH_ONE, null);
+    String dotbuildpath = scriptProject.getProject().getFile(".buildpath")
+      .getRawLocation().toOSString();
 
-    PHPNature nature = (PHPNature)_project.getNature(PHPNature.ID);
-    // not ideal, but does the job.
-    nature.setProject(_project);
-
-    List<String> names =
-      Arrays.asList(PHPProjectOptions.getIncludePathVariableNames());
-
-    PHPProjectOptions options = PHPProjectOptions.forProject(_project);
-    IIncludePathEntry[] entries = options.readRawIncludePath();
-    ArrayList<Error> errs = new ArrayList<Error>();
-    for(IIncludePathEntry entry : entries){
-      String message = entry.validate();
-
-      // perform validation that pdt does not.
-      if (message == null){
-        if (entry.getEntryKind() == IIncludePathEntry.IPE_VARIABLE){
-          String name = entry.getPath().toString();
-          if (!names.contains(name)){
-            message = Services.getMessage("variable.not.found", name);
-          }
-        }else if (entry.getEntryKind() == IIncludePathEntry.IPE_PROJECT){
-          String path = entry.getPath().toString();
-          if(!path.equals("/" + entry.getResource().getName())){
-            message = Services.getMessage("project.path.invalid", path);
-          }
-        }
-      }
-
-      if (message != null){
-        // hacky... using filename arg as the entry path to caculate line/col on
-        // the vim side.
-        errs.add(new Error(message, entry.getPath().toString(), 1, 1, false));
+    IBuildpathEntry[] entries = scriptProject.readRawBuildpath();
+    FileOffsets offsets = FileOffsets.compile(dotbuildpath);
+    String buildpath = IOUtils.toString(new FileInputStream(dotbuildpath));
+    errors = new ArrayList<Error>();
+    for(IBuildpathEntry entry : entries){
+      IModelStatus status = BuildpathEntry.validateBuildpathEntry(
+          scriptProject, entry, true);
+      if(!status.isOK()){
+        errors.add(createErrorFromStatus(offsets, dotbuildpath, buildpath, status));
       }
     }
 
-    return errs;
+    // always set the buildpath anyways, so that the user can correct the file.
+    //if(status.isOK() && errors.isEmpty()){
+      scriptProject.setRawBuildpath(entries, null);
+      scriptProject.makeConsistent(null);
+    //}
+
+    if(errors.size() > 0){
+      return errors;
+    }
+    return null;
   }
 
   /**
@@ -133,19 +149,6 @@ public class PhpProjectManager
   public void refresh (IProject _project, CommandLine _commandLine)
     throws Exception
   {
-    _project.refreshLocal(IResource.DEPTH_INFINITE, null);
-
-    // FIXME: The following clears the dataModel cache properly, but doesn't
-    // reseed it.
-    /*PHPWorkspaceModelManager manager = PHPWorkspaceModelManager.getInstance();
-    manager.removeModel(_project);
-
-    File cacheDir = new File(ProjectUtils.getPath(_project) + "/.cache");
-    if(cacheDir.exists()){
-      FileUtils.deleteDirectory(cacheDir);
-    }
-
-    PHPProjectModel model = manager.getModelForProject(_project, true);*/
   }
 
   /**
@@ -154,5 +157,87 @@ public class PhpProjectManager
   public void delete (IProject _project, CommandLine _commandLine)
     throws Exception
   {
+  }
+
+  /**
+   * Creates an Error from the supplied IModelStatus.
+   *
+   * @param _offsets File offsets for the buildpath file.
+   * @param _filename The filename of the error.
+   * @param _contents The contents of the file as a String.
+   * @param _status The IModelStatus.
+   * @return The Error.
+   */
+  protected Error createErrorFromStatus (
+      FileOffsets _offsets, String _filename, String _contents, IModelStatus _status)
+    throws Exception
+  {
+    int line = 1;
+    int col = 1;
+
+    // get the pattern to search for from the status message.
+    Matcher matcher = STATUS_PATTERN.matcher(_status.getMessage());
+    String pattern = matcher.replaceFirst("$1");
+
+    // find the pattern in the buildpath file.
+    matcher = Pattern.compile("\\Q" + pattern + "\\E").matcher(_contents);
+    if(matcher.find()){
+      int[] position = _offsets.offsetToLineColumn(matcher.start());
+      line = position[0];
+      col = position[1];
+    }
+
+    return new Error(_status.getMessage(), _filename, line, col, false);
+  }
+
+  /**
+   * Creates or updates the projects dependencies on other projects.
+   *
+   * @param _project The project.
+   * @param _depends The comma seperated list of project names.
+   */
+  protected IBuildpathEntry[] createOrUpdateDependencies (
+      IScriptProject _project, String _depends)
+    throws Exception
+  {
+    if(_depends != null){
+      String[] dependPaths = StringUtils.split(_depends, ',');
+      IBuildpathEntry[] entries = new IBuildpathEntry[dependPaths.length];
+      for(int ii = 0; ii < dependPaths.length; ii++){
+        IProject theProject = ProjectUtils.getProject(dependPaths[ii]);
+        if(!theProject.exists()){
+          throw new IllegalArgumentException(Services.getMessage(
+              "project.depends.not.found", dependPaths[ii]));
+        }
+        IScriptProject otherProject = DLTKCore.create(theProject);
+        entries[ii] = DLTKCore.newProjectEntry(otherProject.getPath(), true);
+      }
+      return entries;
+    }
+    return new IBuildpathEntry[0];
+  }
+
+  /**
+   * Merges the supplied buildpath entries into one.
+   *
+   * @param _entries The array of buildpath entry arrays to merge.
+   *
+   * @return The union of all entry arrays.
+   */
+  protected IBuildpathEntry[] merge (IBuildpathEntry[][] _entries)
+  {
+    ArrayList<IBuildpathEntry> union = new ArrayList<IBuildpathEntry>();
+    if(_entries != null){
+      for(IBuildpathEntry[] entries : _entries){
+        if(entries != null){
+          for(IBuildpathEntry entry : entries){
+            if(!union.contains(entry)){
+              union.add(entry);
+            }
+          }
+        }
+      }
+    }
+    return (IBuildpathEntry[])union.toArray(new IBuildpathEntry[union.size()]);
   }
 }
