15f8106c8688b15622a9b074a6aec1658e02ed63
ervandew <ervandew@gmail.com>
2009-01-20 02:43:49 +0000

    don't send all env vars.  handle tcp errors.
    - don't send all env vars to the server.  eclim doesn't need them, so no
      need to include the overhead.
    - when reading response from the server, handle tcp errors to prevent
      infinite loop.
    
    git-svn-id: https://eclim.svn.sourceforge.net/svnroot/eclim/trunk@2328 b5bd0a2e-1a23-0410-8567-9814cbbba304

--- src/nailgun/src/c/ng.c
+++ src/nailgun/src/c/ng.c
@@ -1,4 +1,4 @@
-/*   
+/*
 
   Copyright 2004, Martian Software, Inc.
 
@@ -13,7 +13,7 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-  
+
 */
 
 /**
@@ -32,6 +32,7 @@
 	#include <sys/types.h>
 #endif
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -125,9 +126,9 @@ void handleError () {
   LPVOID lpMsgBuf;
   int error = GetLastError();
 
-  FormatMessage( 
-    FORMAT_MESSAGE_ALLOCATE_BUFFER | 
-    FORMAT_MESSAGE_FROM_SYSTEM | 
+  FormatMessage(
+    FORMAT_MESSAGE_ALLOCATE_BUFFER |
+    FORMAT_MESSAGE_FROM_SYSTEM |
     FORMAT_MESSAGE_IGNORE_INSERTS,
     NULL,
     error,
@@ -141,7 +142,7 @@ void handleError () {
 
   /* Free the buffer. */
   LocalFree( lpMsgBuf );
- 
+
   cleanUpAndExit(error);
 }
 #endif
@@ -157,27 +158,27 @@ void handleError () {
  * @return total bytes written or 0 if failure
  */
 int sendAll(SOCKET s, char *buf, int len) {
-  int total = 0;      
-  int bytesleft = len; 
+  int total = 0;
+  int bytesleft = len;
   int n = 0;
-    
+
   while(total < len) {
     n = send(s, buf+total, bytesleft, 0);
-    
-    if (n == -1) { 
+
+    if (n == -1) {
       break;
     }
-    
+
     total += n;
     bytesleft -= n;
   }
 
-  return n==-1 ? 0:total; 
+  return n==-1 ? 0:total;
 }
 
 /**
  * Sends a chunk header noting the specified payload size and chunk type.
- * 
+ *
  * @param size the payload size
  * @param chunkType the chunk type identifier
  */
@@ -223,14 +224,20 @@ void handleSocketClose() {
 void recvToFD(HANDLE destFD, char *buf, unsigned long len) {
   unsigned long bytesRead = 0;
   int bytesCopied;
-  
+
   while (bytesRead < len) {
     unsigned long bytesRemaining = len - bytesRead;
     int bytesToRead = (BUFSIZE < bytesRemaining) ? BUFSIZE : bytesRemaining;
     int thisPass = 0;
-    
+
     thisPass = recv(nailgunsocket, buf, bytesToRead, MSG_WAITALL);
-   
+
+    /* EV: avoid inifinite loops by handling tcp errors */
+    /* tcp error occurred */
+    if (thisPass == -1 && errno != 0){
+      cleanUpAndExit(errno);
+    }
+
     bytesRead += thisPass;
 
     bytesCopied = 0;
@@ -250,7 +257,7 @@ void recvToFD(HANDLE destFD, char *buf, unsigned long len) {
       #else
         bytesCopied += write(destFD, buf + bytesCopied, thisPass - bytesCopied);
       #endif
-    }  
+    }
   }
 }
 
@@ -266,15 +273,15 @@ void processExit(char *buf, unsigned long len) {
   int exitcode;
   int bytesToRead = (BUFSIZE - 1 < len) ? BUFSIZE - 1 : len;
   int bytesRead = recv(nailgunsocket, buf, bytesToRead, MSG_WAITALL);
-  
+
   if (bytesRead < 0) {
     handleSocketClose();
   }
-  
+
   buf[bytesRead] = 0;
-  
+
   exitcode = atoi(buf);
-  
+
   cleanUpAndExit(exitcode);
 }
 
@@ -295,14 +302,14 @@ void processnailgunstream() {
     if (bytesRead < CHUNK_HEADER_LEN) {
       handleSocketClose();
     }
-  
+
     len = ((buf[0] << 24) & 0xff000000)
       | ((buf[1] << 16) & 0x00ff0000)
       | ((buf[2] << 8) & 0x0000ff00)
       | ((buf[3]) & 0x000000ff);
-  
+
     chunkType = buf[4];
-  
+
     switch(chunkType) {
       case CHUNKTYPE_STDOUT: recvToFD(NG_STDOUT_FILENO, buf, len);
             break;
@@ -386,7 +393,7 @@ int processStdin() {
  */
 void initSockets () {
   WSADATA win_socket_data;     /* required to initialise winsock */
-  
+
   WSAStartup(2, &win_socket_data);
 }
 #endif
@@ -405,11 +412,11 @@ void initIo () {
   securityAttributes.bInheritHandle = TRUE;
   securityAttributes.lpSecurityDescriptor = NULL;
   securityAttributes.nLength = 0;
-  
+
   NG_STDIN_FILENO = GetStdHandle(STD_INPUT_HANDLE);
   NG_STDOUT_FILENO = GetStdHandle(STD_OUTPUT_HANDLE);
   NG_STDERR_FILENO = GetStdHandle(STD_ERROR_HANDLE);
-   
+
   if (!CreateThread(&securityAttributes, 0, &processStdin, NULL, 0, &threadId)) {
     handleError();
   }
@@ -455,7 +462,7 @@ void usage(int exitcode) {
   fprintf(stderr, "          (to execute an aliased class, where \"alias\"\n");
   fprintf(stderr, "           is both the alias for the class and a symbolic\n");
   fprintf(stderr, "           link to the ng client)\n\n");
-  
+
   fprintf(stderr, "where options include:\n");
   fprintf(stderr, "   --nailgun-D<name>=<value>   set/override a client environment variable\n");
   fprintf(stderr, "   --nailgun-version           print product version and exit\n");
@@ -488,19 +495,19 @@ int main(int argc, char *argv[], char *env[]) {
   #ifdef WIN32
   initSockets();
   #endif
-  
+
   /* start with environment variable.  default to localhost if not defined. */
   nailgun_server = getenv("NAILGUN_SERVER");
   if (nailgun_server == NULL) {
     nailgun_server = "127.0.0.1";
   }
-  
+
   /* start with environment variable.  default to normal nailgun port if not defined */
   nailgun_port = getenv("NAILGUN_PORT");
   if (nailgun_port == NULL) {
     nailgun_port = NAILGUN_PORT_DEFAULT;
   }
-  
+
   /* look at the command used to launch this program.  if it was "ng", then the actual
      command to issue to the server must be specified as another argument.  if it
      wasn't ng, assume that the desired command name was symlinked to ng in the user's
@@ -510,10 +517,10 @@ int main(int argc, char *argv[], char *env[]) {
   if (isNailgunClientName(cmd)) {
     cmd = NULL;
   }
-  
+
   firstArgIndex = 1;
 
-  /* quite possibly the lamest commandline parsing ever. 
+  /* quite possibly the lamest commandline parsing ever.
      look for the two args we care about (--nailgun-server and
      --nailgun-port) and NULL them and their parameters after
      reading them if found.  later, when we send args to the
@@ -548,15 +555,15 @@ int main(int argc, char *argv[], char *env[]) {
   if (cmd == NULL) {
     usage(NAILGUN_BAD_ARGUMENTS);
   }
-  
-  /* jump through a series of connection hoops */  
+
+  /* jump through a series of connection hoops */
   hostinfo = gethostbyname(nailgun_server);
 
   if (hostinfo == NULL) {
     fprintf(stderr, "Unknown host: %s\n", nailgun_server);
     cleanUpAndExit(NAILGUN_CONNECT_FAILED);
   }
- 
+
   port = atoi(nailgun_port);
 
   if ((nailgunsocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -564,18 +571,18 @@ int main(int argc, char *argv[], char *env[]) {
     cleanUpAndExit(NAILGUN_SOCKET_FAILED);
   }
 
-  server_addr.sin_family = AF_INET;    
+  server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(port);
   server_addr.sin_addr = *(struct in_addr *) hostinfo->h_addr;
-  
+
   memset(&(server_addr.sin_zero), '\0', 8);
 
   if (connect(nailgunsocket, (struct sockaddr *)&server_addr,
     sizeof(struct sockaddr)) == -1) {
     perror("connect");
     cleanUpAndExit(NAILGUN_CONNECT_FAILED);
-  } 
-    
+  }
+
   /* ok, now we're connected.  first send all of the command line
      arguments for the server, if any.  remember that we may have
      marked some arguments NULL if we read them to specify the
@@ -584,29 +591,30 @@ int main(int argc, char *argv[], char *env[]) {
     if (argv[i] != NULL) sendText(CHUNKTYPE_ARG, argv[i]);
   }
 
-  /* now send environment */  
+  /* now send environment */
   sendText(CHUNKTYPE_ENV, NAILGUN_FILESEPARATOR);
   sendText(CHUNKTYPE_ENV, NAILGUN_PATHSEPARATOR);
-  for(i = 0; env[i]; ++i) {
+  /* EV: eclim doesn't need this, so eliminate the overhead. */
+  /*for(i = 0; env[i]; ++i) {
     sendText(CHUNKTYPE_ENV, env[i]);
-  }
-  
+  }*/
+
   /* now send the working directory */
   cwd = getcwd(NULL, 0);
   sendText(CHUNKTYPE_DIR, cwd);
   free(cwd);
-  
+
   /* and finally send the command.  this marks the point at which
      streams are linked between client and server. */
   sendText(CHUNKTYPE_CMD, cmd);
 
-  
-  /* initialise the std-* handles and the thread to send stdin to the server */ 
+
+  /* initialise the std-* handles and the thread to send stdin to the server */
   #ifdef WIN32
   initIo();
   #endif
 
-  /* stream forwarding loop */	
+  /* stream forwarding loop */
   while(1) {
     #ifndef WIN32
       FD_ZERO(&readfds);
@@ -620,7 +628,7 @@ int main(int argc, char *argv[], char *env[]) {
       if (select (nailgunsocket + 1, &readfds, NULL, NULL, NULL) == -1) {
 	perror("select");
       }
-	  
+
       if (FD_ISSET(nailgunsocket, &readfds)) {
     #endif
 	processnailgunstream();
@@ -632,7 +640,7 @@ int main(int argc, char *argv[], char *env[]) {
 	}
       }
     #endif
-  }  
+  }
 
   /* normal termination is triggered by the server, and so occurs in processExit(), above */
 }
