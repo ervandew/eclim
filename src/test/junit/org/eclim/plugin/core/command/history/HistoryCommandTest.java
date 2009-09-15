b49fee3ead0b9a18b807826b923c8662a24f022e
ervandew <ervandew@gmail.com>
2009-09-14 20:26:45 -0700

    update history delta validation to be more flexible

--- src/test/junit/org/eclim/plugin/core/command/history/HistoryCommandTest.java
+++ src/test/junit/org/eclim/plugin/core/command/history/HistoryCommandTest.java
@@ -40,7 +40,7 @@ public class HistoryCommandTest
   private static final String ENTRY =
     "\\{'timestamp': '(\\d+)'," +
     "'datetime': '\\d\\d:\\d\\d \\w{3} \\w{3} \\d{2} \\d{4}'," +
-    "'delta': '\\d+ (millis|seconds?|minutes?) ago'\\}";
+    "'delta': '(\\d+ (millis|seconds?|minutes?) ){1,}ago'\\}";
 
   /**
    * Test the command.
