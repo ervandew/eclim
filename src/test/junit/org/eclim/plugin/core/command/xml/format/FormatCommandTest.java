c094f5014ebb371493f023fc64a60cb8ee044109
ervandew <ervandew@gmail.com>
2010-02-21 07:53:16 -0800

    fix xml format test for addition of -m arg to the command

--- src/test/junit/org/eclim/plugin/core/command/xml/format/FormatCommandTest.java
+++ src/test/junit/org/eclim/plugin/core/command/xml/format/FormatCommandTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2005 - 2009  Eric Van Dewoestine
+ * Copyright (C) 2005 - 2010  Eric Van Dewoestine
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ public class FormatCommandTest
   {
     String result = Eclim.execute(new String[]{
       "xml_format",
-      "-f", Eclim.resolveFile(TEST_FILE), "-w", "80", "-i", "2"
+      "-f", Eclim.resolveFile(TEST_FILE), "-w", "80", "-i", "2", "-m", "unix"
     });
     System.out.println(result);
 
