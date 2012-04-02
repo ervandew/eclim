c909e73716a782975869c977f4606896ce6bf471
ervandew <ervandew@gmail.com>
2012-04-02 07:13:30 -0700

    fix NPE in settings json validator

--- org.eclim.core/java/org/eclim/plugin/core/preference/JsonValidator.java
+++ org.eclim.core/java/org/eclim/plugin/core/preference/JsonValidator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011  Eric Van Dewoestine
+ * Copyright (C) 2011 - 2012  Eric Van Dewoestine
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -47,7 +47,7 @@ public class JsonValidator
     if (value != null){
       try{
         Object result = GSON.fromJson((String)value, type);
-        if (type.isArray() && itemValidator != null){
+        if (result != null && type.isArray() && itemValidator != null){
           Object[] results = (Object[])result;
           for(Object v : results){
             if (!itemValidator.isValid(v)){
