4ca4c1739cf345fdbc46126046a37c9e3940508c
ervandew <ervandew@gmail.com>
2012-07-04 15:05:12 -0700

    fix ping test for juno

--- org.eclim.core/test/junit/org/eclim/plugin/core/command/admin/PingCommandTest.java
+++ org.eclim.core/test/junit/org/eclim/plugin/core/command/admin/PingCommandTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2005 - 2011  Eric Van Dewoestine
+ * Copyright (C) 2005 - 2012  Eric Van Dewoestine
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -42,6 +42,6 @@ public class PingCommandTest
         Eclim.execute(new String[]{"ping"});
     assertEquals("Unexpected result",
         System.getProperty("eclim.version"), result.get("eclim"));
-    assertTrue("Unexpected result", result.get("eclipse").startsWith("3."));
+    assertTrue("Unexpected result", result.get("eclipse").startsWith("4."));
   }
 }
