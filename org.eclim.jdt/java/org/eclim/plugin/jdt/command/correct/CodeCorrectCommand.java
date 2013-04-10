a4b31c45949f1da7acb955de640cd240bff36b24
ervandew <ervandew@gmail.com>
2013-04-10 07:46:00 -0600

    filter out getter/setter correction proposals
    The getter/setter proposal requires gui interaction which is not
    supported in the headless eclimd.
    
    fixes #209

--- org.eclim.jdt/java/org/eclim/plugin/jdt/command/correct/CodeCorrectCommand.java
+++ org.eclim.jdt/java/org/eclim/plugin/jdt/command/correct/CodeCorrectCommand.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2005 - 2012  Eric Van Dewoestine
+ * Copyright (C) 2005 - 2013  Eric Van Dewoestine
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -105,6 +105,8 @@ public class CodeCorrectCommand
   static {
     IGNORE_BY_INFO.add(CorrectionMessages
         .LocalCorrectionsSubProcessor_InferGenericTypeArguments_description);
+    IGNORE_BY_INFO.add(CorrectionMessages
+        .GetterSetterCorrectionSubProcessor_additional_info);
   }
 
   @Override
