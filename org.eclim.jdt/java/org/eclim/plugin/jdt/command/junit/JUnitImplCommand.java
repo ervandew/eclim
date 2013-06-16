09bc12c065caa455c63cbb3a827799f1979c8c4a
ervandew <ervandew@gmail.com>
2013-06-15 22:30:23 -0600

    fix JUnitUtils.findClass
    - when class is Test*, the mather's group 1 is the class to look for.
      when class is *Test, then use group 2
    - if no type is found, the return null
    
    fixes #225

--- org.eclim.jdt/java/org/eclim/plugin/jdt/command/junit/JUnitImplCommand.java
+++ org.eclim.jdt/java/org/eclim/plugin/jdt/command/junit/JUnitImplCommand.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2005 - 2012  Eric Van Dewoestine
+ * Copyright (C) 2005 - 2013  Eric Van Dewoestine
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -109,7 +109,7 @@ public class JUnitImplCommand
 
     ICompilationUnit test = JavaUtils.getCompilationUnit(javaProject, file);
     ICompilationUnit src = JUnitUtils.findClass(javaProject, test.getTypes()[0]);
-    if (base == null){
+    if (src == null){
       return Services.getMessage("junit.testing.class.not.found");
     }
 
