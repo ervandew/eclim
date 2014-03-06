43e204ffb5602c069ad8ac479e6923fa8635b55b
ervandew <ervandew@gmail.com>
2014-03-06 08:38:06 -0700

    update new scala import support
    - some code style changes
    - move insertion of the import to the server side
    - add unit tests
    
    refs #288

--- org.eclim.sdt/scala/eclim/plugin/sdt/command/complete/CodeCompleteCommand.scala
+++ org.eclim.sdt/scala/eclim/plugin/sdt/command/complete/CodeCompleteCommand.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011 - 2013 Eric Van Dewoestine
+ * Copyright (C) 2011 - 2014 Eric Van Dewoestine
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,6 @@ package eclim.plugin.sdt.command.complete
 
 import java.text.Collator
 
-import java.util.Comparator
 import java.util.Locale
 
 import scala.collection.JavaConversions
