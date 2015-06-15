fa2daebce9c969b7228ede796e1c447d6a31e068
ervandew <ervandew@gmail.com>
2015-06-14 18:45:30 -0600

    fix pydev CodeCompleteCommand for latest pydev api

--- org.eclim.pydev/java/org/eclim/plugin/pydev/command/complete/CodeCompleteCommand.java
+++ org.eclim.pydev/java/org/eclim/plugin/pydev/command/complete/CodeCompleteCommand.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013 - 2014  Eric Van Dewoestine
+ * Copyright (C) 2013 - 2015  Eric Van Dewoestine
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -184,6 +184,13 @@ public class CodeCompleteCommand
     public Object getAdapter(Class adapter)
     {
       return null;
+    }
+
+    @Override
+    public int getGrammarVersion()
+      throws MisconfigurationException
+    {
+      return getPythonNature().getGrammarVersion();
     }
   }
 }
