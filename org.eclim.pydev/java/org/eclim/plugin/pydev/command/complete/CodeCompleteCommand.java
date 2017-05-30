1e213b586c56a434a6217e50be9f8e6bd7dd6671
ervandew <ervandew@gmail.com>
2017-05-30 08:04:01 -0600

    update pydev to 5.7.0

--- org.eclim.pydev/java/org/eclim/plugin/pydev/command/complete/CodeCompleteCommand.java
+++ org.eclim.pydev/java/org/eclim/plugin/pydev/command/complete/CodeCompleteCommand.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013 - 2015  Eric Van Dewoestine
+ * Copyright (C) 2013 - 2017  Eric Van Dewoestine
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -214,6 +214,13 @@ public class CodeCompleteCommand
     @Override
     @SuppressWarnings({"rawtypes", "unchecked"})
     public Object getAdapter(Class adapter)
+    {
+      return null;
+    }
+
+    @Override
+    public AdditionalGrammarVersionsToCheck getAdditionalGrammarVersions()
+      throws MisconfigurationException
     {
       return null;
     }
